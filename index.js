d65be3187e176ac633570b550e4510b97e25549f
Christoph Dorn <christoph@christophdorn.com>
2015-11-03 23:03:18 -0800

    wip

--- index.js
+++ index.js
@@ -11,15 +11,6 @@ module.exports = function(source) {
     var sourceFilename = LOADER_UTILS.getRemainingRequest(self);
     var current = LOADER_UTILS.getCurrentRequest(self);
 
-/*
-  var query = loaderUtils.parseQuery(this.query);
-  if (query.insertPragma) {
-    source = '/ ** @jsx ' + query.insertPragma + ' * /' + source;
-  }
-*/
-
-//console.log("TREANSFORM", sourceFilename, source);
-
     return CVDOM.html2hscript(source, {
         "controlAttributes": {
             "prefix": "component:",
@@ -28,51 +19,9 @@ module.exports = function(source) {
                 "window": true
             }
         }
-    }, function(err, chscript, components, inlineScripts) {
+    }, function(err, chscript, components, inlineScripts, cjsCode) {
         if (err) return self.callback(err);
 
-        var code = [];
-        code.push('module.exports = {');
-        code.push(  'getLayout: function () {');
-        code.push(    'return {');
-        code.push(      'buildVTree: function (h, ch) {');
-        code.push(        'return ' + chscript + ';');
-        code.push(      '}');
-        code.push(    '};');
-        code.push(  '},');
-        code.push(  'getComponents: function () {');
-        code.push(    'return {');
-        Object.keys(components).forEach(function (id, i) {
-            code.push(      (i>0?",":"") + '"' + id + '": {');
-            code.push(        'buildVTree: function (h, ch) {');
-            code.push(          'return ' + components[id].chscript + ';');
-            code.push(        '}');
-            code.push(      '}');
-        });
-        code.push(  '  };');
-        code.push(  '},');
-        code.push(  'getScripts: function () {');
-        code.push(    'return ' + JSON.stringify(inlineScripts) + ';');
-        code.push(  '}');
-        code.push('};');
-
-        self.callback(null, code.join("\n"), {});
+        self.callback(null, cjsCode, {});
     });
-    
-
-/*
-  var transform = jstransform.transform(source, {
-    react: true,
-    harmony: query.harmony,
-    stripTypes: query.stripTypes,
-    es5: query.es5,
-    sourceMap: this.sourceMap
-  });
-  if (transform.sourceMap) {
-    transform.sourceMap.sources = [sourceFilename];
-    transform.sourceMap.file = current;
-    transform.sourceMap.sourcesContent = [source];
-  }
-  this.callback(null, transform.code, transform.sourceMap);
-*/
 };
